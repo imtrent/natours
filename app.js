dd3b93170c1591cb9af01a64f4fd4049aee0e734
Ian Trent <ian.m.trent@gmail.com>
2019-08-03 19:55:53 -0400

    Added morgan package integration

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Middlewares
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     console.log('Hello from the middleware 👋');
