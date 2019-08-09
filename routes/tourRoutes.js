a447bf213a88f6caf7f6610dd20a9344e487c2cc
Ian Trent <ian.m.trent@gmail.com>
2019-08-09 19:08:48 -0400

    Changed tab spacing to 4 for better readability

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,14 +7,14 @@ const router = express.Router();
 // router.param('id', tourController.checkID);
 
 router
-  .route('/')
-  .get(tourController.getAllTours)
-  .post(tourController.createTour);
+	.route('/')
+	.get(tourController.getAllTours)
+	.post(tourController.createTour);
 
 router
-  .route('/:id')
-  .get(tourController.getTourById)
-  .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+	.route('/:id')
+	.get(tourController.getTourById)
+	.patch(tourController.updateTour)
+	.delete(tourController.deleteTour);
 
 module.exports = router;
