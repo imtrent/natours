54784768d9e59bc6df8e852ad70aedbbf0ccb345
Ian Trent <ian.m.trent@gmail.com>
2019-10-02 21:15:05 -0400

    Added error handling for unhandled rejections

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ mongoose
     });
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Listening on port ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.errmsg);
+    console.log('UNHANDLED REJECTION! 💥 Shutting down');
+    server.close(() => {
+        process.exit(1);
+    });
 });
