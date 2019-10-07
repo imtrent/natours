75813b7774089a228bb9f2465330597d32a1dba5
Ian Trent <ian.m.trent@gmail.com>
2019-10-06 20:28:13 -0400

    Added passwordChangedAt field to user model

--- models/userModel.js
+++ models/userModel.js
@@ -31,7 +31,8 @@ const userSchema = new mongoose.Schema({
             },
             message: 'Passwords do not match'
         }
-    }
+    },
+    passwordChangedAt: Date
 });
 
 userSchema.pre('save', async function(next) {
@@ -46,6 +47,26 @@ userSchema.pre('save', async function(next) {
 
     next();
 });
+
+userSchema.methods.correctPassword = async function(
+    candidatePassword,
+    userPassword
+) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+    if (this.passwordChangedAt) {
+        const changedTimestamp = parseInt(
+            this.passwordChangedAt.getTime() / 1000,
+            10
+        );
+        console.log(changedTimestamp, JWTTimestamp);
+        return JWTTimestamp < changedTimestamp;
+    }
+    // False means that the password was not changed
+    return false;
+};
 
 const User = mongoose.model('User', userSchema);
 
