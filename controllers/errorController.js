30b527562aae63dec29406b267ab70f556761658
Ian Trent <ian.m.trent@gmail.com>
2019-10-06 20:28:42 -0400

    Added error handling for JWT token error and expiration error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -18,6 +18,11 @@ const handleValidationErrorDB = err => {
     return new AppError(message, 400);
 };
 
+const handleJWTError = () => new AppError('Invalid token. Please login.', 401);
+
+const handleJWTExpiredError = () =>
+    new AppError('Your login session has expired! Please login again.', 401);
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -60,7 +65,9 @@ module.exports = (err, req, res, next) => {
         if (error.code === 11000) error = handleDuplicateFieldsDB(error);
         if (error.name === 'ValidationError')
             error = handleValidationErrorDB(error);
-
+        if (error.name === 'JsonWebTokenError') error = handleJWTError(error);
+        if (error.name === 'TokenExpiredError')
+            error = handleJWTExpiredError(error);
         sendErrorProd(error, res);
     }
 };
