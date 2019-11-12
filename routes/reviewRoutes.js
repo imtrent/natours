6d1a37a366c80152d2352b2868149a8359fc6ab4
Ian Trent <ian.m.trent@gmail.com>
2019-11-11 21:33:18 -0500

    Setup more secure route protection on review, tour, and user routes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,13 +4,27 @@ const reviewController = require('./../controllers/reviewController');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+
 router
     .route('/')
     .get(reviewController.getAllReviews)
     .post(
-        authController.protect,
         authController.restrictTo('user'),
+        reviewController.setTourUserIds,
         reviewController.createReview
+    );
+
+router
+    .route('/:id')
+    .get(reviewController.getReview)
+    .patch(
+        authController.restrictTo('user', 'admin'),
+        reviewController.updateReview
+    )
+    .delete(
+        reviewController.deleteReview,
+        authController.restrictTo('user', 'admin')
     );
 
 module.exports = router;
