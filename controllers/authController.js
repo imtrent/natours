1092caa9e7c132e32f25598e09021a732c491766
Ian Trent <ian.m.trent@gmail.com>
2019-11-24 20:38:09 -0500

    Started login functionality on client side

--- controllers/authController.js
+++ controllers/authController.js
@@ -76,6 +76,8 @@ exports.protect = catchAsync(async (req, res, next) => {
         req.headers.authorization.startsWith('Bearer')
     ) {
         token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies.jwt) {
+        token = req.cookies.jwt;
     }
 
     if (!token) {
@@ -110,6 +112,33 @@ exports.protect = catchAsync(async (req, res, next) => {
 
     // Grant access to the protected route
     req.user = currentUser;
+    next();
+});
+
+// Only for rendered pages, no errors!
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+    if (req.cookies.jwt) {
+        // 1) Verify token (if someone manipulated it, or if it has expired)
+        const decoded = await promisify(jwt.verify)(
+            req.cookies.jwt,
+            process.env.JWT_SECRET
+        );
+
+        // 2) Check if user still exists
+        const currentUser = await User.findById(decoded.id);
+        if (!currentUser) {
+            return next();
+        }
+
+        // 3) Check if user changed password after the token was issued
+        if (currentUser.changedPasswordAfter(decoded.iat)) {
+            return next();
+        }
+
+        // If gets to this point, there is a logged in user
+        res.locals.user = currentUser;
+        return next();
+    }
     next();
 });
 
