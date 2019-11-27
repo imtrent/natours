56350eabdd7a1590b83b15ed63ab89227500c57e
Ian Trent <ian.m.trent@gmail.com>
2019-11-26 20:32:14 -0500

    Added client side functionality for updating user data

--- public/js/index.js
+++ public/js/index.js
@@ -2,15 +2,19 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
+import { updateData } from './updateData';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
 // VALUES
 if (mapBox) {
-    const locations = JSON.parse(document.getElementById('map').dataset.locations);
+    const locations = JSON.parse(
+        document.getElementById('map').dataset.locations
+    );
     displayMap(locations);
 }
 
@@ -18,12 +22,20 @@ if (mapBox) {
 if (loginForm) {
     loginForm.addEventListener('submit', e => {
         e.preventDefault();
-    
+
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
-        
+
         login(email, password);
     });
 }
 
 if (logoutBtn) logoutBtn.addEventListener('click', logout);
+
+if (userDataForm)
+    userDataForm.addEventListener('submit', e => {
+        e.preventDefault();
+        const email = document.querySelector('#email').value;
+        const name = document.querySelector('#name').value;
+        updateData(name, email);
+    });
